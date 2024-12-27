c57888a1921de27c997bbeb10f8fc827e14600d2
kms3563 <71589939+kms3563@users.noreply.github.com>
2024-12-27 16:42:37 +0900

    Update kocom.py

--- kocom.py
+++ kocom.py
@@ -682,7 +682,7 @@ def publish_discovery(dev, sub=''):
             'curr_temp_t': 'kocom/room/thermo/{}/state'.format(num),
             'curr_temp_tpl': '{{ value_json.cur_temp }}',
             'modes': ['off', 'heat'],
-            'min_temp': 20,
+            'min_temp': 16,
             'max_temp': 30,
             'ret': 'false',
             'qos': 0,
